9d61adaef85f60ceb6c995b3920b9bba9c9ebb75
Ana Marques <absilvamarques@gmail.com>
2026-06-05 12:01:39 -0300

    feat: adiciona códigos aprendidos da página 5 à 11

--- capitulo_01.R
+++ capitulo_01.R
@@ -1,10 +1,43 @@
 # CAPÍTULO 1 - VISUALIZAÇÃO DE DADOS COM GGPLOT2
 library(tidyverse)
 
-# carros com motores maiores usam mais combustível que carros com motores menores?
-# com o que a relação entre tamanho de motor e eficácia de co combustível se parece?
-# é positivo? negativo? linear? não linear?
-
+# pag 5
 ggplot(data = mpg)+
   geom_point(mapping = aes(x = displ, y = hwy))
+
+# pag 8
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy, colour = class))
+
+# pag 9 
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy, size = class))
+
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy, alpha = class))
+
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy, shape = class))
+
+# pag 11
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy), size = "blue")
+
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy), size = 3)
+
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy), shape = 2)
+
+# size = c(0, 14), borda com color
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy), shape = 1, color = "blue")
+
+# size = c(15, 18), preenche com color
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy), shape = 15, color = "blue")
+
+# size = c(19, 24), borda com color e preenche com fill
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy), shape = 21, color = "blue", fill = "red")
 
