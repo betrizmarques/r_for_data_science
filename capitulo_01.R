9bd88e439ddd8ea27c615fd853eeddf9e15f14ad
Ana Marques <absilvamarques@gmail.com>
2026-06-05 23:25:37 -0300

    feat: adiciona códigos das páginas 16 à 20.

--- capitulo_01.R
+++ capitulo_01.R
@@ -66,3 +66,51 @@ ggplot(data = mpg)+
 ggplot(data = mpg)+
   geom_point(mapping = aes(x = displ, y = hwy))+
   facet_grid(. ~ cyl)
+
+# pag 16
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg)+
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+# pag 17
+ggplot(data = mpg)+
+  geom_smooth(mapping = aes(x = displ, y = hwy, linetype = drv))
+
+ggplot(data = mpg)+
+  geom_smooth(mapping = aes(x = displ, y = hwy, linetype = drv))+
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+# pag 18
+ggplot(data = mpg)+
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg)+
+  geom_smooth(mapping = aes(x = displ, y = hwy, group = drv))
+
+ggplot(data = mpg)+
+  geom_smooth(
+    mapping = aes(x = displ, y = hwy, color = drv),
+    show.legend = FALSE)
+
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy))+
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+# pag 19
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy))+
+  geom_point()+
+  geom_smooth()
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy))+
+  geom_point(mapping = aes(color = class))+
+  geom_smooth()
+
+# pag 20
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy))+
+  geom_point(mapping = aes(color = class))+
+  geom_smooth(
+    data = filter(mpg, class == "subcompact"),
+    se = FALSE
+  )
