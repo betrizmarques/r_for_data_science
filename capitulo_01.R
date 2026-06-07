57850ca4ed36fd90f02cc2abd3bbab07efdbee55
Ana Marques <absilvamarques@gmail.com>
2026-06-06 21:56:54 -0300

    feat: adiciona parte de Transformações Estatísticas (#pag 22)

--- capitulo_01.R
+++ capitulo_01.R
@@ -114,3 +114,40 @@ ggplot(data = mpg, mapping = aes(x = displ, y = hwy))+
     data = filter(mpg, class == "subcompact"),
     se = FALSE
   )
+
+# pag 22
+ggplot(data = diamonds)+
+  geom_bar(mapping = aes(x = cut))
+
+?geom_bar
+
+# pag 23
+ggplot(data = diamonds)+
+  stat_count(mapping = aes(x = cut))
+
+# pag 24
+demo <- tribble(
+  ~a, ~b,
+  "bar_1", 20,
+  "bar_2", 30,
+  "bar_3", 40
+)
+
+ggplot(data = demo)+
+  geom_bar(
+    mapping = aes(x = a, y = b), stat = "identity"
+  )
+
+# pag 25
+ggplot(data = diamonds)+
+  geom_bar(
+    mapping = aes(x = cut, y = after_stat(prop), group = 1)
+    )
+
+ggplot(data = diamonds)+
+  stat_summary(
+    mapping = aes(x = cut, y = depth),
+    fun.ymin = min,
+    fun.ymax = max,
+    fun.y = median
+  )
