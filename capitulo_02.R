d0088d8f65fc9e95b5f09060085d034a291a583b
Ana Marques <absilvamarques@gmail.com>
2026-06-08 23:48:13 -0300

    Capítulo 02: adiciona códigos de Fluxo de Trabalho - O básico

---
+++ capitulo_02.R
@@ -0,0 +1,24 @@
+# CAPÍTULO 2 - FLUXO DE TRABALHO: O BÁSICO
+
+# pag 37
+1 / 200 * 30
+(59 + 73 + 2) / 3
+sin(pi / 2)
+x <- 3 * 4
+
+# pag 38
+x
+este_eh_um_nome_bem_longo <- 2.5
+este_eh_um_nome_bem_longo
+r_rocks <- 2 ** 3
+r_rock
+
+# pag 39
+seq(1, 10)
+x <- "hello world"
+x <- "hello"
+
+# pag 40
+y <- seq(1, 10 , length.out = 5)
+y
+(y <- seq(1, 10 , length.out = 5))
